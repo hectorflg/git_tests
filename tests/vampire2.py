151c3abfaa4f0ff837160a2ac8ce76ccab5ea464
Hector Rafael <hectogar@cisco.com>
2020-04-05 16:51:10 -0500

    New point of start

--- tests/vampire2.py
+++ tests/vampire2.py
@@ -8,12 +8,3 @@ elif age > 100:
     print("You are not Alice, grannie")
 elif age > 2000:
         print("Unlike you, Alice is not an undead, immortal vampire.")
-<<<<<<< HEAD
-elif age < 4000:
-        print("You are a little vampire.")
-<<<<<<< HEAD
-#This message will be deleted
-=======
->>>>>>> parent of 099d04a... Modifying vampire2.py adding one more elif
-=======
->>>>>>> parent of 86afae4... Adding a comment to vampire2.py
