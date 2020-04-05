442c5e81686d71865c796b7551163a5ce37d0a37
Hector Rafael <hectogar@cisco.com>
2020-04-05 16:41:35 -0500

    Rollback to commit 099d04

--- tests/vampire2.py
+++ tests/vampire2.py
@@ -8,6 +8,9 @@ elif age > 100:
     print("You are not Alice, grannie")
 elif age > 2000:
         print("Unlike you, Alice is not an undead, immortal vampire.")
+<<<<<<< HEAD
 elif age < 4000:
         print("You are a little vampire.")
 #This message will be deleted
+=======
+>>>>>>> parent of 099d04a... Modifying vampire2.py adding one more elif
